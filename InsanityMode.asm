2264642d4e95506e077c327181f3db0cfcc7e535
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-13 19:16:35 -0400

    Fixed some errors. (You don't wanna know.)

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,4 +1,4 @@
 //Insanity Mode among with Rotation Wind and Wind Direction evenly.
 org $80025C00
-li  t1,8    ///<summary> Protects it from crashing it. ///</summary>
+li  t1, $0008    ///<summary> Protects it from crashing it. ///</summary>
 SW, S3 $0054(SP)
