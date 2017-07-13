c123d2709db2b5d2b7b1f212a3072c2ed3353bf7
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-13 19:48:57 -0400

    Update InsanityMode.asm

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,5 +1,5 @@
 //Insanity Mode among with Rotation Wind and Wind Direction evenly.
-org $80025C00
-lui t1,  $0000    ///<summary> Protects it from crashing it. ///</summary>
-ori t1, t1  $0008
+.org $80025C00
+SW, RA, 0x14(SP)    ///<summary> Protects it from crashing it. ///</summary>
+lui T0,  0x8033
 SW, S3 $0054(SP)
