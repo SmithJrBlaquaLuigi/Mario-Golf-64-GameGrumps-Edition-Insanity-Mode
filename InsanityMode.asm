c00e0531ee47e90ec1ba48062bd73bad16ab219d
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-13 19:41:03 -0400

    Added ori to add a function

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,4 +1,5 @@
 //Insanity Mode among with Rotation Wind and Wind Direction evenly.
 org $80025C00
-li  t1, $0008    ///<summary> Protects it from crashing it. ///</summary>
+lui t1,  $0000    ///<summary> Protects it from crashing it. ///</summary>
+ori t1, t1  $0008
 SW, S3 $0054(SP)
