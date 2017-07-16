b6f7c76f2b7eaa307474510fbf5b1d0013907de3
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-16 15:10:43 -0400

    Update entry point message

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,5 +1,5 @@
 //Insanity Mode among with Rotation Wind and Wind Direction evenly.
-.org $80025C00
+.org $80025C00    //Entry Point begins at 0x80025C00
 SW, RA, 0x14(SP)    ///<summary> Protects it from crashing it. ///</summary>
 lui T0,  0x8033
 SW, S3 $0054(SP)
