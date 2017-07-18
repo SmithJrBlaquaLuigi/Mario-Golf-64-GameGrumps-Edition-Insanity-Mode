ea982de067bc3a72bb7ffc364f796867c32b121d
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-17 23:58:12 -0400

    Update InsanityMode.asm

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,5 +1,4 @@
 //Insanity Mode among with Rotation Wind and Wind Direction evenly.
-<<<<<<< HEAD
 org $80025C00
 li  t1,8    // Protects it from crashing it.
 SW, S3 $0054(SP)
@@ -17,9 +16,7 @@ DIV R19, R2
 SLL R2, R2, $2
 NOP
 ADDIU SP, SP, 0x18
-=======
 .org $80025C00    //Entry Point begins at 0x80025C00
 SW, RA, 0x14(SP)    ///<summary> Protects it from crashing it. ///</summary>
 lui T0,  0x8033
 SW, S3 $0054(SP)
->>>>>>> b6f7c76f2b7eaa307474510fbf5b1d0013907de3
