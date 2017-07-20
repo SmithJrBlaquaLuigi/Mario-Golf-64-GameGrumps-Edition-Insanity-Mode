2e239cb386be4b8c2b6719f056796964b0fab0dd
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-20 01:03:08 -0400

    ROM offset that we need to write into that address.

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -2,7 +2,5 @@
 .org $80025C00
 
 //Wind Speed
-.orga 0xECA179 //This is the ROM offset that we need to write it on.
+.orga 0xECA179 //This is the ROM offset that we need to write into.
 LUI T0, $8034
-.
-
