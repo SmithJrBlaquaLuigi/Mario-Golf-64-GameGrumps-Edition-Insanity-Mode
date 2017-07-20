3a4d4210e7877c1d0b9478d28af974d706156d22
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-20 03:33:06 -0400

    Finished adding Wind Speed code, maybe?

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -5,6 +5,10 @@
 .orga 0xECA179 //This is the ROM offset that we need to write into.
 LUI T0, $8034
 ADDIU RA, RA, $5B60
+SD R0, $5D60 (T8)
+NOP
+JR, R0 $0000
+SD T1, $5D60(T8)
 
 //code for Wind Direction
 .orga 0xECA17C
