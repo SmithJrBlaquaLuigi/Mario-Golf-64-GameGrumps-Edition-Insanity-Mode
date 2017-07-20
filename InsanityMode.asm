cd33c11b45579588af17e755b0a011c8de29dccd
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-20 16:09:05 -0400

    .

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,14 +1,9 @@
 //Entry Point.. blah.
-.org $80025C00
+.org 0x80025C00
 
 //Wind Speed
 .orga 0xECA179 //This is the ROM offset that we need to write into.
-LUI T0, $8034
-ADDIU RA, RA, $5B60
-SD R0, $5D60 (T8)
-NOP
-JR, R0 $0000
-SD T1, $5D60(T8)
-
+LUI A0, 0x0000
+ORI A1, A1, 0x0001
 //code for Wind Direction
 .orga 0xECA17C
