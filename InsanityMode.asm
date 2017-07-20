1e5c9025a9d84799f913077b2c1363d5f31f70e2
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-20 00:43:36 -0400

    added ROM address for Wind speed but not complied yet.

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,25 +1,6 @@
-//Insanity Mode among with Rotation Wind and Wind Direction evenly.
-<<<<<<< HEAD
-org $80025C00
-li  t1,8    // Protects it from crashing it.
-SW, S3 $0054(SP)
-SW R0, $0034(R2)
-ADDIU R3, R2, $0030
-LUI R3, $E700
-LW R8, $D650 (R8)
-NOP
-ORI R5, R5, $0100
-BNE R3, R2, $000CBDA8
-LW R3, $4408(R3)
-ADDU R5, R30, R0
-BEQ R3, R2, $000BAA4C
-DIV R19, R2
-SLL R2, R2, $2
-NOP
-ADDIU SP, SP, 0x18
-=======
-.org $80025C00    //Entry Point begins at 0x80025C00
-SW, RA, 0x14(SP)    ///<summary> Protects it from crashing it. ///</summary>
-lui T0,  0x8033
-SW, S3 $0054(SP)
->>>>>>> b6f7c76f2b7eaa307474510fbf5b1d0013907de3
+//Entry Point.. blah.
+.org $80025C00
+
+//Wind Speed
+.orga 0xECA179 //This is the ROM offset that we need to write it on.
+LUI T0, $8034
