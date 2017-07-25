56b0c95cfeb5f39f60ac808f03db2ed73fea4a16
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-25 18:05:17 -0400

    No.

--- InsanityMode.asm
+++ InsanityMode.asm
@@ -1,12 +1,13 @@
 //Entry Point, Wind speed and Wind Direction...
 .org 0xBA9F0
-ADDIU SP, SP, -0xD0
+ADDIU SP, SP, -0x18
+NOP
 SW RA, 0x14(SP)
 
-LW V1, 0xA9F0(V1)
+LW A0, 0xA9F0 (A0)
 ORI A0, A1, 0x0001
-JAL 0x800A7B20
+NOP
 
 LW RA, 0x14(SP)
 JR RA
-ADDIU SP, SP, 0x18
+ADDIU SP, SP, -0x18
